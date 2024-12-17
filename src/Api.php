bc26ce6483f751dabf5031eae7d8b8c0d287a31e
ArtOsi7 <arturonthemail@gmail.com>
2024-12-17 11:27:55 +0200

    remove unused code

--- src/Api.php
+++ src/Api.php
@@ -3,10 +3,6 @@
 
 class Api
 {
-    const REQUIRED_PARAMS = ['reservation_date', 'reservation_from', 'reservation_time', 'car_number'];
-
-    private $errors = [];
-
     /**
      * @var ReservationsManager $reservationsManager
      */
@@ -19,47 +15,13 @@ class Api
 
     public function processRequest(string $requestData)
     {
-       // $this->validateRequestData($requestData);
         if (!$this->isValidJson($requestData)) {
             $this->JsonResponse(['errors' => ['date is invalid json format']]);
         }
-        //print_r($requestData); exit;
+
         $processedReservationsData = $this->reservationsManager->processData(json_decode($requestData, true));
 
         $this->JsonResponse($processedReservationsData);
-    }
-
-    private function validateRequestData($data)
-    {
-        if (!$this->isValidJson($data)) {
-            $this->JsonResponse(['errors' => ['date is invalid json format']]);
-        }
-
-        $data = json_decode($data, true);
-        //@TODO: check if multidimensional array (multiple reservations)
-        foreach (self::REQUIRED_PARAMS as $param) {
-            if (!isset($data[$param])) {
-                $this->errors[] = 'Missing param ' . $param;
-            }
-        }
-
-        if ($this->errors) {
-            $this->JsonResponse(['errors' => $this->errors]);
-        }
-    }
-
-    private function validateDate(string $date, string $format = 'Y-m-d H:i')
-    {
-        $date = DateTime::createFromFormat($format, $date);
-        // check if date format is valid
-        if ($date === false) {
-            $this->errors[] = 'Invalid date';
-        } else {
-            //check if date is not in the past
-            if (new DateTime() > $date) {
-                $this->errors[] = 'Date is in the past';
-            }
-        }
     }
 
     /**
